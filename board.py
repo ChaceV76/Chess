250ed449d27b5b2193422514a01e1e10b9bd08a9
ChaceV76 <chacevang2006@gmail.com>
2026-01-31 17:26:11 -0600

    Constructed a Squares class to create square objects for our board and giving them coordinates. Now what we need to work on is construcing a loop to add rows with the elements containing the specific squares in that row and iterating over it 64 times and moving to a new row after every 8th iteration.

--- board.py
+++ board.py
@@ -1,5 +1,6 @@
 '''This file will be used to generate the board and pieces visually'''
 import pygame
+import numpy as np
 
 # Sets up pygaame "Turning on Engine"
 pygame.init()
@@ -12,21 +13,42 @@ running = True
 test_surface = pygame.Surface((640, 640))
 test_surface.fill('lightsalmon4')
 
-# Create black "squares"
-square = pygame.Rect((80, 80), (80, 80))
+
+# Make a class for these squares and get their coordinates
+class Squares():
+    def __init__(self, x: int, y: int):
+        self.x = int(x)
+        self.y = int(y)
+        self.rect = pygame.Rect((self.x, self.y), (80, 80))
+
+    def get_x(self):
+        return self.x
+    
+    def get_y(self):
+        return self.y
+    
+# Create a 2D Numpy array to give us rows and columns
+chess_board = np.array([])
+    
+num_objects = 64
+
+# This loop will create a new row with the square objects
+for i in range(num_objects):
+    new_square = Squares(0, 0)
+    chess_board.vstack(new_square)
+
+pygame.draw.rect(test_surface, 'navajowhite', A8.rect)
 
 
-pygame.draw.rect(test_surface, 'navajowhite', square)
+
 
 '''
 # Make a loop that skips over to the right by 80 width and fills a 80x80 navajo white rectangle after each 4th iteration move down a line
-for index, square in enumerate(range(64), start=1):
-    if index % 2 == 0: # Draw the square then start at the next row, x-axis should be the same 0, but y moves down + 80
-        square.move(80, 0)
-    else:
-        pass
+for index, _ in enumerate(range(63)):
+    x = square.copy()
+    x.move(80, 0)
+    pygame.draw.rect(test_surface, 'navajowhite', x)
 '''
-
 
 while running:
     # pygame.QUIT event means the user clicked X to close your window
